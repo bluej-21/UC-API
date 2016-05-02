6d700413bd34b5be129d086a20b8c5ba27cb6a21
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-05-02 10:52:05 -0700

    structure

---
+++ Scrapers/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='uc-api scrapers'
+    version=scrapers.__version__,
+    url='http://github.com/bluej-21/uc-api/',
+    author='Nagee Elghassein',
+    author_email='nag3el@gmail.com',
+)
