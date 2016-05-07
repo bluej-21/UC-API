b6570cb6f4bafd7176d635a93f3814544c32e425
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-05-07 15:20:27 +0300

    fixed get td text

--- Scrapers/scrapers/UCLA/courses/course.py
+++ Scrapers/scrapers/UCLA/courses/course.py
@@ -1,18 +1,29 @@
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
+
+def _depth_first_html_search(html, level, levelDic):
+    for node in html.children:
+        if isinstance(node, NavigableString):
+            if node.string:
+                if level in levelDic.keys():
+                    levelDic[level].append(node.string)
+                else:
+                    levelDic[level] = [node.string]
+            return None
+        _depth_first_html_search(node, level + 1, levelDic)
+    return None
+
+def get_text(tree):
+    text_dict = {}
+    level = 0
+
+    _depth_first_html_search(tree, level, text_dict)
+    return text_dict
 
 def get_td_text(td):
-    for c in td.contents:
-        try:
-            if c.get_text().strip('\n'):
-                return c.get_text()
-        except AttributeError as e:
-            try:
-                if c.span.a.get_text().strip('\n'):
-                    return c.span.get_text()
-            except AttributeError as e:
-                pass
-
-    return ''
+    text = get_text(td)
+    max_depth = max(text.keys())
+    return text[max_depth][0]
 
 def get_course_data():
     data = open('course.html','r').read()
@@ -21,15 +32,11 @@ def get_course_data():
     datas = []
     for table in tables:
         data = []
+
         # get table header
         rows = table.find_all('tr')
-        headers = [td.text.strip() for td in rows[0].find_all('td')]
-        for row in rows[1:]:
-            row_html = [get_td_text(td) for td in row.find_all('td')]
-            row_data = zip(headers, row_html)
-            data.append(row_data)
-        datas.append(data)
-    return datas
+        header = [get_td_text(td) for td in rows[0].find_all('td')]
 
-print get_course_data()
+        # still only two rows getting caputred
 
+get_course_data()
